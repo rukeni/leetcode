39eb32c777c50e7e58a643ffd43039237ba9fa1a
양탕구리 <rukeni@naver.com>
2021-10-26 21:31:14 +0900

    Time: 64 ms (100.00%), Space: 40.5 MB (44.32%) - LeetHub

---
+++ minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} seats
+ * @param {number[]} students
+ * @return {number}
+ */
+var minMovesToSeat = function(seats, students) {
+  let count = 0;
+  seats.sort((a,b) => a - b);
+  students.sort((a,b) => a - b);
+  for (let i = 0; i < seats.length; i++) {
+    count += Math.abs(seats[i] - students[i])
+  }
+  return count;
+};
