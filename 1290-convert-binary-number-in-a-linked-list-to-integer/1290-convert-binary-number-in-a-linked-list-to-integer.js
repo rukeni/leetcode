821aea94481ca0597e0f61c77308e50e2f6b985f
양탕구리 <rukeni@naver.com>
2022-04-26 17:26:39 +0900

    Time: 119 ms (5.27%), Space: 44.2 MB (5.13%) - LeetHub

---
+++ 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number}
+ */
+var getDecimalValue = function(head) {
+  const result = [];
+  while(head.next !== null) {
+    console.log(head.val)
+    result.push(head.val);
+    head = head.next;
+  }
+  result.push(head.val)
+  return parseInt(result.join(''), 2)
+};
