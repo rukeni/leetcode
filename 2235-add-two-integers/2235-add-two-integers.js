a3ffd078f50596dd743a1dc445533724bf0fb22b
rukeni <rukeni@naver.com>
2022-09-29 22:05:59 +0900

    Time: 116 ms (10.18%), Space: 42.1 MB (50.72%) - LeetHub

--- 2235-add-two-integers/2235-add-two-integers.js
+++ 2235-add-two-integers/2235-add-two-integers.js
@@ -4,5 +4,10 @@
  * @return {number}
  */
 function sum (a, b) {
-  return a + b;
+  if( a === 0) return b;
+  if( b === 0) return a;
+  
+  var _sum = a ^ b;
+  var _carry = (a&b) << 1;
+  return sum(_sum, _carry)
 }
